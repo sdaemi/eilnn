006acbd192f0f8a1a84879bfbdce5e67b35eecb9
Tom Tranter <t.g.tranter@gmail.com>
2021-12-13 16:23:28 +0000

    Add setup, move files to a subfolder with __init__,  update readme and add gitignore

---
+++ eilnn/__init__.py
@@ -0,0 +1,10 @@
+"""
+# eilnn
+
+A tool for segmenting and classifying particle data from tomography images of
+battery electrodes using neural networks
+"""
+from .data_import_benchmark import *
+from .tools import *
+
+__version__ = "0.0.1"
