be99754306664443cb64e4d689e5a8c8073c41d6
DELL90-PS-2\Sohrab <sohrab.daemi@gmail.com>
2022-01-18 12:58:12 +0000

    build check

--- eilnn/__init__.py
+++ eilnn/__init__.py
@@ -5,7 +5,6 @@ A tool for segmenting and classifying particle data from tomography images of
 battery electrodes using neural networks
 """
 from .annotation_creator import *
-from .tools import *
 from .utils import *
 
 
